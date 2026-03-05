e9dfccc22350baa721419ccce565639e30c97fbe
Marian <bzov.06@gmail.com>
2026-03-06 00:01:19 +0200

    додали новий функціонал

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ const products = [
     {
         id: 1,
         name: "Iphone 15 pro",
+        device: "iphone",
+        model: "iPhone 15 Pro",
+        memory: "256GB",
+        condition: "new",
         brand: "Apple",
         category: "Smartphone",
         price: 1200,
@@ -12,6 +16,10 @@ const products = [
     {
         id: 1,
         name: "MacBook air",
+        device: "iphone",
+        model: "iPhone 15 Pro",
+        memory: "256GB",
+        condition: "new",
         brand: "Apple",
         category: "Laptop",
         price: 1500,
@@ -22,6 +30,10 @@ const products = [
     {
         id: 1,
         name: "Samsung A56",
+        device: "iphone",
+        model: "iPhone 15 Pro",
+        memory: "256GB",
+        condition: "new",
         brand: "Samsung",
         category: "Smartphone",
         price: 900,
@@ -32,6 +44,10 @@ const products = [
     {
         id: 1,
         name: "AppleWatch",
+        device: "iphone",
+        model: "iPhone 15 Pro",
+        memory: "256GB",
+        condition: "new",
         brand: "Apple",
         category: "Smartphone",
         price: 600,
@@ -42,18 +58,408 @@ const products = [
     {
         id: 1,
         name: "Ipad",
+        device: "iphone",
+        model: "iPhone 15 Pro",
+        memory: "256GB",
+        condition: "new",
         brand: "Apple",
         category: "Smartphone",
         price: 700,
         color: "Black",
         img: "https://tabletklasse.de/wp-content/uploads/iPad-10-blau-TeacherStore.png"
     },
+
+    {
+        id: 1,
+        name: "Iphone 15 pro",
+        device: "iphone",
+        model: "iPhone 15 Pro",
+        memory: "256GB",
+        condition: "new",
+        brand: "Apple",
+        category: "Smartphone",
+        price: 1200,
+        color: "Black",
+        img: "https://pisces.bbystatic.com/image2/BestBuy_US/images/products/6525/6525407_sd.jpg"
+    },
+
+    {
+        id: 2,
+        name: "Iphone 16 pro",
+        device: "iphone",
+        model: "iPhone 15 Pro",
+        memory: "256GB",
+        condition: "new",
+        brand: "Apple",
+        category: "Smartphone",
+        price: 1300,
+        color: "Silver",
+        img: "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone-16-pro-model-unselect-gallery-1-2024"
+    },
+
+    {
+        id: 3,
+        name: "Iphone 17 pro",
+        device: "iphone",
+        model: "iPhone 15 Pro",
+        memory: "256GB",
+        condition: "new",
+        brand: "Apple",
+        category: "Smartphone",
+        price: 1400,
+        color: "Gray",
+        img: "https://www.apple.com/newsroom/images/product/iphone/standard/Apple-iPhone-17-Pro-hero.jpg"
+    },
+
+    {
+        id: 4,
+        name: "MacBook Air M3",
+        device: "iphone",
+        model: "iPhone 15 Pro",
+        memory: "256GB",
+        condition: "new",
+        brand: "Apple",
+        category: "Laptop",
+        price: 1500,
+        color: "Black",
+        img: "https://9to5mac.com/wp-content/uploads/sites/6/2025/03/macbook-air-m4.jpg?quality=82&strip=all&w=1600"
+    },
+
+    {
+        id: 5,
+        name: "MacBook Pro M3",
+        device: "iphone",
+        model: "iPhone 15 Pro",
+        memory: "256GB",
+        condition: "new",
+        brand: "Apple",
+        category: "Laptop",
+        price: 2200,
+        color: "Space Gray",
+        img: "https://www.apple.com/v/macbook-pro/images/overview/hero_endframe__cfpfg03g32eu_large.jpg"
+    },
+
+    {
+        id: 6,
+        name: "Samsung Galaxy A56",
+        device: "iphone",
+        model: "iPhone 15 Pro",
+        memory: "256GB",
+        condition: "new",
+        brand: "Samsung",
+        category: "Smartphone",
+        price: 900,
+        color: "Black",
+        img: "https://images.samsung.com/is/image/samsung/assets/global/mkt/smartphones/galaxy-a56/buy/02_Product_Image_MO.png?imbypass=true"
+    },
+
+    {
+        id: 7,
+        name: "Samsung Galaxy S24",
+        device: "iphone",
+        model: "iPhone 15 Pro",
+        memory: "256GB",
+        condition: "new",
+        brand: "Samsung",
+        category: "Smartphone",
+        price: 1100,
+        color: "Blue",
+        img: "https://images.samsung.com/is/image/samsung/p6pim/levant/sm-s921blbgmea/gallery/levant-galaxy-s24-sm-s921-sm-s921blbgmea-539218022"
+    },
+
+    {
+        id: 8,
+        name: "Apple Watch Series 11",
+        device: "iphone",
+        model: "iPhone 15 Pro",
+        memory: "256GB",
+        condition: "new",
+        brand: "Apple",
+        category: "Smartwatch",
+        price: 600,
+        color: "Black",
+        img: "https://www.apple.com/v/apple-watch-series-11/b/images/overview/product-viewer/product_landing_endframe__eaytrp6zz6c2_large.jpg"
+    },
+
+    {
+        id: 9,
+        name: "Apple Watch Ultra 2",
+        device: "iphone",
+        model: "iPhone 15 Pro",
+        memory: "256GB",
+        condition: "new",
+        brand: "Apple",
+        category: "Smartwatch",
+        price: 900,
+        color: "Titanium",
+        img: "https://www.apple.com/v/apple-watch-ultra-2/images/overview/hero_endframe__d9u2xzz2z7ue_large.jpg"
+    },
+
+    {
+        id: 10,
+        name: "Ipad 10",
+        device: "iphone",
+        model: "iPhone 15 Pro",
+        memory: "256GB",
+        condition: "new",
+        brand: "Apple",
+        category: "Tablet",
+        price: 700,
+        color: "Blue",
+        img: "https://tabletklasse.de/wp-content/uploads/iPad-10-blau-TeacherStore.png"
+    },
+
+    {
+        id: 11,
+        name: "Ipad 11",
+        device: "iphone",
+        model: "iPhone 15 Pro",
+        memory: "256GB",
+        condition: "new",
+        brand: "Apple",
+        category: "Tablet",
+        price: 850,
+        color: "Silver",
+        img: "https://www.apple.com/v/ipad/images/overview/hero_endframe__cs7d6c3x7x2q_large.jpg"
+    },
+
+    {
+        id: 12,
+        name: "Samsung Galaxy Tab S9",
+        device: "iphone",
+        model: "iPhone 15 Pro",
+        memory: "256GB",
+        condition: "new",
+        brand: "Samsung",
+        category: "Tablet",
+        price: 950,
+        color: "Gray",
+        img: "https://images.samsung.com/is/image/samsung/p6pim/global/sm-x710nzaamea/gallery/global-galaxy-tab-s9-sm-x710-sm-x710nzaamea-537829693"
+    },
+
+    {
+        id: 13,
+        name: "Iphone 14",
+        device: "iphone",
+        model: "iPhone 15 Pro",
+        memory: "256GB",
+        condition: "new",
+        brand: "Apple",
+        category: "Smartphone",
+        price: 900,
+        color: "White",
+        img: "https://www.apple.com/v/iphone-14/images/overview/hero/hero_iphone14__fjmsqstr1ceq_large.jpg"
+    },
+
+    {
+        id: 14,
+        name: "Iphone 15",
+        device: "iphone",
+        model: "iPhone 15 Pro",
+        memory: "256GB",
+        condition: "new",
+        brand: "Apple",
+        category: "Smartphone",
+        price: 1050,
+        color: "Blue",
+        img: "https://www.apple.com/v/iphone-15/images/overview/hero/hero_iphone15__fsaosn2pb8ae_large.jpg"
+    },
+
+    {
+        id: 15,
+        name: "Iphone 16",
+        device: "iphone",
+        model: "iPhone 15 Pro",
+        memory: "256GB",
+        condition: "new",
+        brand: "Apple",
+        category: "Smartphone",
+        price: 1150,
+        color: "Pink",
+        img: "https://www.apple.com/v/iphone-16/images/overview/hero/hero_iphone16__d1r0k1k7c6u6_large.jpg"
+    },
+
+    {
+        id: 16,
+        name: "MacBook Air M2",
+        device: "iphone",
+        model: "iPhone 15 Pro",
+        memory: "256GB",
+        condition: "new",
+        brand: "Apple",
+        category: "Laptop",
+        price: 1300,
+        color: "Silver",
+        img: "https://www.apple.com/v/macbook-air-m2/images/overview/hero/hero_macbookair_m2__ejbsjj8nf6oi_large.jpg"
+    },
+
+    {
+        id: 17,
+        name: "MacBook Pro M2",
+        device: "iphone",
+        model: "iPhone 15 Pro",
+        memory: "256GB",
+        condition: "new",
+        brand: "Apple",
+        category: "Laptop",
+        price: 2000,
+        color: "Space Gray",
+        img: "https://www.apple.com/v/macbook-pro-13/images/overview/hero/hero_endframe__bsza6x4fldiq_large.jpg"
+    },
+
+    {
+        id: 18,
+        name: "Ipad Pro 11",
+        device: "iphone",
+        model: "iPhone 15 Pro",
+        memory: "256GB",
+        condition: "new",
+        brand: "Apple",
+        category: "Tablet",
+        price: 1100,
+        color: "Gray",
+        img: "https://www.apple.com/v/ipad-pro/images/overview/hero/hero_ipadpro__d5fg7uq8b6uq_large.jpg"
+    },
+
+    {
+        id: 19,
+        name: "Ipad Pro 12.9",
+        device: "iphone",
+        model: "iPhone 15 Pro",
+        memory: "256GB",
+        condition: "new",
+        brand: "Apple",
+        category: "Tablet",
+        price: 1400,
+        color: "Silver",
+        img: "https://www.apple.com/v/ipad-pro/images/overview/hero/hero_ipadpro_129__g9t7x2p7p6um_large.jpg"
+    },
+
+    {
+        id: 20,
+        name: "Samsung Galaxy S23",
+        device: "iphone",
+        model: "iPhone 15 Pro",
+        memory: "256GB",
+        condition: "new",
+        brand: "Samsung",
+        category: "Smartphone",
+        price: 950,
+        color: "Green",
+        img: "https://images.samsung.com/is/image/samsung/p6pim/global/sm-s911bzggeub/gallery/global-galaxy-s23-sm-s911-sm-s911bzggeub-535889509"
+    },
+
+    {
+        id: 21,
+        name: "Samsung Galaxy S25",
+        device: "iphone",
+        model: "iPhone 15 Pro",
+        memory: "256GB",
+        condition: "new",
+        brand: "Samsung",
+        category: "Smartphone",
+        price: 1250,
+        color: "Black",
+        img: "https://images.samsung.com/is/image/samsung/assets/global/galaxy-s25/galaxy-s25_highlights_kv.jpg"
+    },
+
+    {
+        id: 22,
+        name: "Samsung Galaxy Z Flip 6",
+        device: "iphone",
+        model: "iPhone 15 Pro",
+        memory: "256GB",
+        condition: "new",
+        brand: "Samsung",
+        category: "Smartphone",
+        price: 1400,
+        color: "Purple",
+        img: "https://images.samsung.com/is/image/samsung/p6pim/global/sm-f741blbamea/gallery/global-galaxy-z-flip6-sm-f741-sm-f741blbamea-539084028"
+    },
+
+    {
+        id: 23,
+        name: "AirPods Pro 2",
+        device: "iphone",
+        model: "iPhone 15 Pro",
+        memory: "256GB",
+        condition: "new",
+        brand: "Apple",
+        category: "Headphones",
+        price: 300,
+        color: "White",
+        img: "https://www.apple.com/v/airpods-pro/images/overview/hero/hero_airpodspro__ezc9to6tj6oi_large.jpg"
+    },
+
+    {
+        id: 24,
+        name: "AirPods Max",
+        device: "iphone",
+        model: "iPhone 15 Pro",
+        memory: "256GB",
+        condition: "new",
+        brand: "Apple",
+        category: "Headphones",
+        price: 550,
+        color: "Black",
+        img: "https://www.apple.com/v/airpods-max/images/overview/hero/hero_airpods_max__f3g5jwnkz2m2_large.jpg"
+    },
+
+    {
+        id: 25,
+        name: "Apple TV 4K",
+        device: "iphone",
+        model: "iPhone 15 Pro",
+        memory: "256GB",
+        condition: "new",
+        brand: "Apple",
+        category: "Electronics",
+        price: 200,
+        color: "Black",
+        img: "https://www.apple.com/v/apple-tv-4k/images/overview/hero/hero_appletv4k__bqjt6yy2j5ua_large.jpg"
+    }
+
 ]
 const grid = document.getElementById("productGrid");
 const findBtn = document.getElementById("findBtn");
 const countLabel = document.getElementById("count");
 const priceRange = document.getElementById("priceFilter");
 const priceValueLabel = document.getElementById("priceValue");
+const deviceFilter = document.getElementById("deviceFilter");
+const modelFilter = document.getElementById("modelFilter");
+const statusFilter = document.getElementById("statusFilter");
+
+
+
+const formatDevice = (device) =>
+    device === "iphone" ? "iPhone" : device === "ipad" ? "iPad" : device;
+
+const formatCondition = (condition) =>
+    condition === "new" ? "Новий" : condition === "used" ? "Б/У" : condition;
+
+const populateModelOptions = () => {
+    if (!modelFilter || !deviceFilter) {
+        return;
+    }
+
+    const selectedDevice = deviceFilter.value;
+    const models = products
+        .filter((p) => selectedDevice === "all" || p.device === selectedDevice)
+        .map((p) => p.model);
+
+    const uniqueModels = [...new Set(models)].sort((a, b) => a.localeCompare(b));
+    const previousValue = modelFilter.value;
+
+    modelFilter.innerHTML = '<option value="all">Всі моделі</option>';
+    uniqueModels.forEach((model) => {
+        modelFilter.insertAdjacentHTML(
+            "beforeend",
+            `<option value="${model}">${model}</option>`,
+        );
+    });
+
+    modelFilter.value = uniqueModels.includes(previousValue) ? previousValue : "all";
+};
 
 priceRange.addEventListener("input", () => {
     priceValueLabel.textContent = priceRange.ariaValue;
@@ -87,10 +493,14 @@ const render = (data) => {
         const html = `
     <div class="product-card">
        <img src="${p.img}" alt="${p.name}">
-       <div style="font-size: 12px; color: var(--accent)">${p.brand}</div>
-       <h3>${p.name}</h3>
-       <p style="color: var(--text-dim); font-size: 14px">Колір: ${p.color}</p>
-       <p class="price">$${p.price}</p>
+         <div style="font-size: 12px; color: var(--accent)">${p.brand}</div>
+          <div style="font-size: 12px; color: var(--accent)">${formatDevice(p.device)}</div>
+         <h3>${p.name}</h3>
+        <p style="color: var(--text-dim); font-size: 14px">Колір: ${p.color}</p>
+          <p style="color: var(--text-dim); font-size: 14px">Модель: ${p.model}</p>
+          <p style="color: var(--text-dim); font-size: 14px">Пам'ять: ${p.memory}</p>
+          <p style="color: var(--text-dim); font-size: 14px">Стан: ${formatCondition(p.condition)}</p>
+        <p class="price">$${p.price}</p>
        <button class="search-btn" style="padding: 8px; font-size: 14px">У кошик</button>
     </div>
     `;
@@ -108,14 +518,21 @@ const applyFilters = () => {
     ).value;
     const categoryFilter = document.getElementById("categoryFilter");
     const catQuery = categoryFilter ? categoryFilter.value : "all";
+    const deviceQuery = deviceFilter ? deviceFilter.value : "all";
+    const modelQuery = modelFilter ? modelFilter.value : "all";
+    const statusQuery = statusFilter ? statusFilter.value : "all";
     const maxPrice = Number(priceRange.value);
     const sortOrderSelect = document.getElementById("sortOrder");
     const sortOrder = sortOrderSelect ? sortOrderSelect.value : "none";
 
+    // Вибираємо тільки відмічені значення пам'яті
+    const selectedMemory = Array.from(
+        document.querySelectorAll(".memory-filter:checked"),
+    ).map((cb) => cb.value);
+
     // const catQuery = document.getElementById("categoryFilter").value;
     // const maxPrice = Number(priceRange.value);
     // const sortOrder = document.getElementById("sortOrder").value;
-
     const selectedColors = Array.from(
         document.querySelectorAll(".color-filter:checked"),
     ).map((cb) => cb.value);
@@ -126,17 +543,24 @@ const applyFilters = () => {
         const matchCat = catQuery === "all" || p.category === catQuery;
         const matchPrice = p.price <= maxPrice;
         const matchColor = selectedColors.length === 0 || selectedColors.includes(p.color);
+        const matchDevice = deviceQuery === "all" || p.device === deviceQuery;
+        const matchModel = modelQuery === "all" || p.model === modelQuery;
+        const matchMemory =
+            selectedMemory.length === 0 || selectedMemory.includes(p.memory);
+        const matchCondition = statusQuery === "all" || p.condition === statusQuery;
 
-        return matchName && matchBrand && matchCat && matchPrice && matchColor;
+        return (
+            matchDevice &&
+            matchModel &&
+            matchMemory &&
+            matchCondition &&
+            matchPrice &&
+            matchName &&
+            matchBrand &&
+            matchCat &&
+            matchColor
+        );
     });
-
-    if (sortOrder === "lowToHigh") {
-        filtered.sort((a, b) => a.price - b.price);
-    } else if (sortOrder === "higtToLow") {
-        filtered.sort((a, b) => b.price - a.price);
-    }
-
-    render(filtered);
 
     if (sortOrder === "lowToHigh") {
         filtered.sort((a, b) => a.price - b.price);
@@ -145,9 +569,13 @@ const applyFilters = () => {
     }
     render(filtered)
 }
++
 
+    findBtn.addEventListener("click", applyFilters);
 
-findBtn.addEventListener("click", applyFilters);
+deviceFilter.addEventListener("change", populateModelOptions);
+populateModelOptions();
 
 render(products)
+
 
